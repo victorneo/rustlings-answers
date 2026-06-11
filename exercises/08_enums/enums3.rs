ad8647d077a9e9da4c62705d7727274753a19512
Victor Neo <icyisamu@gmail.com>
2026-06-11 22:59:54 +0800

    08 done

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -46,6 +46,13 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        match message {
+            Message::Resize { width, height } => self.resize(width, height),
+            Message::Move(p) => self.move_position(p),
+            Message::Echo(message) => self.echo(message),
+            Message::ChangeColor(a, b, c) => self.change_color(a, b, c),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
